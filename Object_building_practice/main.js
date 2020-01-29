92b63b8e0a078f0b33863927c4839f24c8f08aca
0pippi0 <j.romany91@gmail.com>
2020-01-29 15:05:53 +0900

    객체 구축 실습
    충돌 감지 추가
    
    각 공마다 다른 모든 공을 확인하여 현재 공과 충돌했는지 확인해야합니다. 이를 위해 배열의 for모든 볼을 반복하기 위해 다른 루프를 시작 합니다 balls[].
    for 루프 내부에서, 우리 if는 루프를 통해 현재 볼이 현재 확인중인 볼과 같은지 확인 하기 위해 명령문을 사용합니다 . 우리는 공이 자신과 충돌했는지 여부를 확인하고 싶지 않습니다! 이를 위해 현재 볼 (즉, 충돌 감지 메소드가 호출되는 볼)이 루프 볼 (즉, 충돌 감지에서 for 루프의 현재 반복에서 참조되는 볼)과 동일한 지 확인합니다. 방법). 그런 다음 !검사를 무효화하여 if명령문 내의 코드 가 동일 하지 않은 경우에만 실행되도록합니다 .
    그런 다음 공통 알고리즘을 사용하여 두 원의 충돌을 확인합니다. 우리는 기본적으로 두 원의 영역 중 하나가 겹치는 지 확인합니다. 이것은 2D 충돌 감지 에서 더 설명 됩니다.
    충돌이 감지되면 내부 if명령문 의 코드 가 실행됩니다. 이 경우 color두 원 의 속성 만 새로운 임의의 색상으로 설정합니다. 우리는 공이 서로 현실적으로 튀어 오르게하는 것과 같이 훨씬 더 복잡한 것을 할 수 있었지만, 구현하기가 훨씬 더 복잡했을 것입니다. 이러한 물리 시뮬레이션의 경우 개발자는 PhysicsJS , matter.js , Phaser 등과 같은 게임 또는 물리 라이브러리를 사용하는 경향이 있습니다

--- Object_building_practice/main.js
+++ Object_building_practice/main.js
@@ -53,6 +53,21 @@ Ball.prototype.update = function() {
   this.y += this.velY;
 }
 
+// define ball collision detection
+Ball.prototype.collisionDetect = function() {
+  for (let j=0; j<balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+      }
+    }
+  }
+}
+
 // define array to store balls and populate it
 let balls = [];
 
