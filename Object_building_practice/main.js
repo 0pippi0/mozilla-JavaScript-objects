a4df093a8382abf47edaa6491f10954778bc23bd
0pippi0 <j.romany91@gmail.com>
2020-01-29 13:53:47 +0900

    객체 구축 실습
    우리 프로그램에서 공 모델링
    
    물체로 공을 나타내는 것
    
    x및 y좌표-화면에서 공이 시작되는 가로 및 세로 좌표입니다. 범위는 0 (왼쪽 상단)에서 브라우저 뷰포트의 너비와 높이 (오른쪽 하단) 사이입니다.
    수평 및 수직 속도 ( velX및 velY)-각 공에 수평 및 수직 속도가 부여됩니다. 실제로 볼 때 이러한 값은 볼에 애니메이션을 적용 할 때 마다 x/ y좌표 값에 정기적으로 추가되어 각 프레임에서 이만큼 많이 움직입니다.
    color — 각 공은 색깔을 얻습니다.
    size — 각 공은 크기를 얻습니다. 이것은 반경 단위의 픽셀입니다.

--- Object_building_practice/main.js
+++ Object_building_practice/main.js
@@ -8,8 +8,17 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 // function to generate random number
-
 function random(min,max) {
   const num = Math.floor(Math.random()*(max-min)) + min;
   return num;
+}
+
+// define Ball constructor
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
 }
